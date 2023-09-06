2799ec51b18e2d9f218e10275e6da1f75fba6913
yuvraj <yuvraj042003@gmail.com>
2023-09-06 18:46:30 +0530

    Add

---
+++ App.js
@@ -0,0 +1,10 @@
+const heading = React.createElement(
+  "h1",
+  {
+    id: "h13",
+    xyz: "abc",
+  },
+  "Hello React "
+);
+const root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(heading);
